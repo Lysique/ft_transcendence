3868e75c79dbe15e49226f0449bff049a660b9ee
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-08-09 14:25:19 +0200

    User resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigService } from './config/typeorm/typeorm.service';
+import { UsersModule } from './models/users/users.module';
 
 //  Get the environment file
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
@@ -12,7 +13,9 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 //  Import environment variables with config modules and configure typeorm
 @Module({
   imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true }),
-    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService })],
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
