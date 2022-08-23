473021bd498b4beda0cf5badaaf3352cb185f65c
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-08-23 11:15:46 +0200

    Cleaned ProfileImage with children props

--- frontend/src/route/Profile.tsx
+++ frontend/src/route/Profile.tsx
@@ -1,15 +1,25 @@
+import { Card } from "@mui/material";
 import React from "react";
+import { UserContext } from "../App";
+import ProfileImageButtons from "../components/profile/profileImage/ProfileImageButtons";
 import ProfileImage from "../components/profile/profileImage/ProfileImage";
 import ProfileName from "../components/profile/profileName/ProfileName";
 
 export const Profile = () => {
 
+    const user = React.useContext(UserContext);
+
     return (
-        <div>
-            <ProfileImage
-            />
-            <ProfileName
-            />
-        </div>
+        <>
+        <Card sx={{ maxWidth: 240, mt: 3, ml: 3 }}>
+            <ProfileImageButtons>
+                <ProfileImage
+                    profileImage={user?.currentAvatar}
+                />
+            </ProfileImageButtons>
+        </Card>
+        <ProfileName
+        />
+        </>
     );
 }
