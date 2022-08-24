24d9cae5fac2d786956293cb71ddc061c92e36ec
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-08-24 09:07:41 +0200

    Managing little filou taking other people's intra name

--- frontend/src/components/auth/UpdateUserName.tsx
+++ frontend/src/components/auth/UpdateUserName.tsx
@@ -1,7 +1,13 @@
-import { Button } from "@mui/material";
-import React from "react";
-import { UserAPI } from "../../api/user.api";
-import { SetUserContext } from "../../App";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { SetUserContext } from '../../App';
+import { UserAPI } from '../../api/user.api';
 
 export const UpdateUserName = () => {
 
@@ -19,13 +25,34 @@ export const UpdateUserName = () => {
             setUser(resp);
         }
     };
+  
+    const handleCancel = () => {
+      setUser(null);
+    };
 
     return (
         <>
-            <input type="text" onChange={handleInput}></input>
-            <Button onClick={updateName}>
-                Choose name
-            </Button>
+            <Dialog open={true}>
+                <DialogTitle>Subscribe</DialogTitle>
+                <DialogContent>
+                <DialogContentText>
+                    A little filou has taken your intra username! :shokedface:
+                    Please choose an other one.
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleInput}
+                />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCancel}>Cancel</Button>
+                    <Button onClick={updateName}>Set name</Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
