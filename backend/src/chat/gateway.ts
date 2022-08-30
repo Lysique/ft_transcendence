8f57e648b9f352c2b04744c7b6e2cc9bf8e76aef
Pierre Pizzo <ppizzo@mi-r5-p1.s19.be>
2022-08-30 14:09:31 +0200

    capitalize friendlist.tsx

---
+++ backend/src/chat/gateway.ts
@@ -0,0 +1,42 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket } from 'dgram';
+import { setServers } from 'dns';
+import { SocketAddress } from 'net';
+import { identity } from 'rxjs';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+})
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()//listen to default api port
+  server: Server;//socket instance
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {//listen to connection, d'instance socket
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+
+  //client consuming websocket server
+  //chat send event to socket
+  //it suscribes to this event
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {//onMessage event
+      msg: 'New Message',//all the payload
+      content: body,
+      //socketid: this.server.sockets
+    });
+  }
+}
