4798e6460e065ea0396f890cff69bc04a613bd8e
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-09-14 13:45:02 +0200

    added type games api

--- frontend/src/api/game.api.ts
+++ frontend/src/api/game.api.ts
@@ -3,7 +3,7 @@ import { Game } from "interfaces/gameInterfaces";
 
 export class GameAPI {
   
-    public static async getGameSessions() {
+    public static async getGameSessions(): Promise<{games: Game[]} | null> {
       const resp = await fetch(`http://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}/game`, {
         credentials: "include",
         method: "GET"
