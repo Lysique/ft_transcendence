7a1bed9a7f6e784458fdb8e75960eddd0b5e236d
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-09-27 09:28:28 +0200

    add files

---
+++ frontend/src/components/chat/ChatSettings.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Menu from '@mui/material/Menu';
+import Tooltip from "@mui/material/Tooltip";
+
+interface ChatSettingsProps {
+    handleOpenSettings: any
+    handleCloseSettings: any
+    settings: HTMLElement
+  }
+
+export const ChatSettings = ({handleOpenSettings, handleCloseSettings, settings}: ChatSettingsProps) => {
+
+    
+    return (
+            <Box sx={{display:'flex', flexDirection: 'row-reverse', marginRight:'15px'}}>
+            <Tooltip title="Settings">
+                <IconButton onClick={handleOpenSettings} sx={{ p: 0 }}>
+                    <SettingsIcon fontSize='large'/>
+                </IconButton>
+            </Tooltip>
+            <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={settings}
+            anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+            }}
+            open={Boolean(settings)}
+            onClose={handleCloseSettings}
+            >
+                <div>LEAVE THE CHANNEL</div>
+                {  true ?
+                <div>PASWORD SETTINGS</div>
+                : ''
+                }
+            </Menu>
+        </Box>
+
+    );
+}
